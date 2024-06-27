6c223b894e994c596a4fecc75b895cc953caefc5
helven <helvenk@foxmail.com>
2024-06-27 23:44:02 +0800

    Update airchains.sh

--- airchains.sh
+++ airchains.sh
@@ -277,13 +277,6 @@ function delete_node() {
 function main_menu() {
   while true; do
     clear
-    echo "脚本由大赌社区candy编写，推特 @ccaannddyy11，免费开源，请勿相信收费"
-    echo "特别鸣谢 @TestnetCn @y95277777 @EthExploring"
-    echo "================================================================"
-    echo "节点社区 Telegram 群组:https://t.me/niuwuriji"
-    echo "节点社区 Telegram 频道:https://t.me/niuwuriji"
-    echo "节点社区 Discord 社群:https://discord.gg/GbMV5EcNWF"
-    echo "退出脚本，请按键盘ctrl c退出即可"
     echo "请选择要执行的操作:"
     echo "1. 安装节点"
     echo "2. 查看wasmstationd状态"
