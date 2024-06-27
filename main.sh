7516d5fe6ad3182bfcf676a6e30e3017df6878bb
helven <helvenk@foxmail.com>
2024-06-28 04:08:34 +0800

    nubit

--- main.sh
+++ main.sh
@@ -6,23 +6,19 @@ function main_menu() {
     clear
     echo "=========两岸猿声啼不住，轻舟已过万重山。========="
     echo "请选择项目:"
-    echo "--------------------节点类项目--------------------"
-    echo "000. Airchains 一键部署"
-    echo "--------------------挖矿类项目--------------------"
-    echo "503. Spectre(CPU) 一键挖矿"
-    echo "110. Titan Network 一键挖矿"
-    echo "---------------------已停项目---------------------"
-    echo "107. Taiko 一键部署[已停用]"
-    echo "501. ORE(CPU) -v1 挖矿脚本[已停用]"
-    echo "502. ORE(GPU) -v1 挖矿脚本[已停用]"
-    echo "101. Babylon 一键部署"
-    echo "-----------------------其他----------------------"
+    echo "--------------------节点类--------------------"
+    echo "000. Airchains"
+    echo "001. Nubit"
+    echo "--------------------挖矿类--------------------"
+    echo "--------------------已停用---------------------"
+    echo "---------------------其他----------------------"
     echo "0. 退出脚本"
     read -p "请输入选项: " OPTION
 
     case $OPTION in
 
     000) wget -O airchains.sh https://raw.githubusercontent.com/helvenk/node_scripts/master/airchains.sh && chmod +x airchains.sh && ./airchains.sh ;;
+    001) wget -O nubit.sh https://raw.githubusercontent.com/a3165458/nubit.sh/main/nubit.sh && chmod +x nubit.sh && ./nubit.sh ;;
 
     0)
       echo "退出脚本。"
